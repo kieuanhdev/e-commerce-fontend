fd1bfcf1243c6ae1ffad90636efcb04e5ba13e3e
Kieuanhdev <kieuanh.dev@gmail.com>
2025-11-23 23:00:04 +0700

    Refactor Categories  URL Search Params

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,6 +1,15 @@
-"use client"
+"use client";
 
-import { ShoppingBasket, Shirt, Footprints, Glasses, Briefcase, Venus, Hand } from "lucide-react"
+import {
+    ShoppingBasket,
+    Shirt,
+    Footprints,
+    Glasses,
+    Briefcase,
+    Venus,
+    Hand,
+} from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
 const categories = [
     {
@@ -42,21 +51,42 @@ const categories = [
         name: "Gloves",
         icon: <Hand className="w-4 h-4" />,
         slug: "gloves",
-    }
-]
-
+    },
+];
 
 const Categories = () => {
+    const searchParams = useSearchParams();
+
+    const router = useRouter();
+
+    const pathname = usePathname();
+
+    const selectedCategory = searchParams.get("category");
+
+    const handleChange = (value: string | null) => {
+        const params = new URLSearchParams(searchParams);
+        params.set("category", value || "all");
+        router.push(`${pathname}?${params.toString()}`, { scroll: false });
+    };
+
     return (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm'>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm">
             {categories.map((category) => (
-                <div key={category.slug} className='flex items-center gap-2 justify-center p-2 bg-white rounded-md shadow-sm hover:shadow-md cursor-pointer transition'>
+                <div
+                    key={category.slug}
+                    className={`flex items-center gap-2 justify-center px-2 py-1 rounded-md cursor-pointer hover:bg-gray-200 transition-colors
+                    ${selectedCategory === category.slug
+                            ? "bg-white"
+                            : "text-gray-600"
+                        }`}
+                    onClick={() => handleChange(category.slug)}
+                >
                     {category.icon}
                     <p>{category.name}</p>
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default Categories
+export default Categories;
