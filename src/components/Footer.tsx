709e19f79275cf05951ba8538b870bb0ec0367c7
Kieuanhdev <kieuanh.dev@gmail.com>
2025-11-23 21:21:25 +0700

    Refactor Footer component to include branding and navigation links; enhance layout and styling for better responsiveness.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,42 @@
+import Link from "next/link"
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <div className=''>Footer</div>
+    <div className='mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:gap-0 md:justify-between bg-gray-800 p-8 rounded-lg'>
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        <Link href="/" className="flex items-center">
+          <Image src="/logo.png" alt="TrendLama" width={36} height={36} className="w-6 h-6 md:w-9 md:h-9" />
+          <p className="hidden md:block text-md font-medium tracking-wider text-white" >TRENDLAMA.</p>
+        </Link>
+        <p className="text-sm text-gray-400">2025 Trendlamda</p>
+        <p className="text-sm text-gray-400">All rights reserved</p>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+      </div>
+
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-sm text-amber-50">Links</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Privacy Policy</Link>
+      </div>
+    </div>
   )
 }
 
