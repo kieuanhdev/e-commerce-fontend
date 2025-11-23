ebaccd233c1c0ef2f6d8350785c7ea04efaa106b
Kieuanhdev <kieuanh.dev@gmail.com>
2025-11-23 22:29:51 +0700

    Implement Categories component with dynamic category listing and icons

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,9 +1,62 @@
 "use client"
 
+import { ShoppingBasket, Shirt, Footprints, Glasses, Briefcase, Venus, Hand } from "lucide-react"
+
+const categories = [
+    {
+        name: "All",
+        icon: <ShoppingBasket className="w-4 h-4" />,
+        slug: "all",
+    },
+    {
+        name: "T-shirts",
+        icon: <Shirt className="w-4 h-4" />,
+        slug: "t-shirts",
+    },
+    {
+        name: "Shoes",
+        icon: <Footprints className="w-4 h-4" />,
+        slug: "shoes",
+    },
+    {
+        name: "Accessories",
+        icon: <Glasses className="w-4 h-4" />,
+        slug: "accessories",
+    },
+    {
+        name: "Bags",
+        icon: <Briefcase className="w-4 h-4" />,
+        slug: "bags",
+    },
+    {
+        name: "Dresses",
+        icon: <Venus className="w-4 h-4" />,
+        slug: "dresses",
+    },
+    {
+        name: "Jackets",
+        icon: <Shirt className="w-4 h-4" />,
+        slug: "jackets",
+    },
+    {
+        name: "Gloves",
+        icon: <Hand className="w-4 h-4" />,
+        slug: "gloves",
+    }
+]
+
+
 const Categories = () => {
-  return (
-    <div className=''>Categories</div>
-  )
+    return (
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm'>
+            {categories.map((category) => (
+                <div key={category.slug} className='flex items-center gap-2 justify-center p-2 bg-white rounded-md shadow-sm hover:shadow-md cursor-pointer transition'>
+                    {category.icon}
+                    <p>{category.name}</p>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Categories
