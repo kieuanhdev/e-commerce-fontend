8d25b5ad4f0d3ac178140e2f924b9263d4c8e371
Kieuanhdev <kieuanh.dev@gmail.com>
2025-11-24 22:40:59 +0700

    feat: Product Filter

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,6 +1,8 @@
 import type { ProductsType } from "@/types";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Link from "next/link";
+import Filter from "./Filter";
 
 const products: ProductsType = [
     {
@@ -125,15 +127,21 @@ const products: ProductsType = [
     },
 ];
 
-const ProductList = () => {
+const ProductList = ({category, params}: {category: string, params:"homepage" | "products"}) => {
     return (
         <div className="w-full">
             <Categories />
+            {params === "products" && <Filter />}
             <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-12">
                 {products.map((product) => (
                     <ProductCard key={product.id} product={product} />
                 ))}
             </div>
+            <Link 
+            href={category ? `/products/?category=${category}` : "/products" }
+            className="flex justify-end mt-4 underline text-sm text-gray-600"
+            >
+                View all product</Link>
         </div>
     );
 };
